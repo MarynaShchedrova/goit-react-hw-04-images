37bd052fbc5aaed2c1f3abba34229fa17831d293
MarynaShchedrova <shchedrovamaryna@gmail.com>
2023-05-14 20:42:27 +0300

    update

---
+++ src/components/api/fetchImages.jsx
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = `https://pixabay.com/api`;
+
+export const fetchImages = async (inputValue, pageNr) => {
+  const response = await axios.get(
+    `/?q=${inputValue}&page=${pageNr}&key=36382390-097dd70cda73be850080c8e98&image_type=photo&orientation=horizontal&per_page=12`
+  );
+  return response.data.hits.map(image => {
+    return {
+      id: image.id,
+      webformatURL: image.webformatURL,
+      largeImageURL: image.largeImageURL,
+      tags: image.tags,
+    };
+  });
+};
